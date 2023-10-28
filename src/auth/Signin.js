ffc08270569afc5c2262702b87d2afe1d51efcab
junggwanho <rhksgh1222@naver.com>
2023-10-28 18:11:37 +0900

    id pw pw2 css 수정

--- src/auth/Signin.js
+++ src/auth/Signin.js
@@ -4,8 +4,27 @@ function Signin(props) {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [idErrorMessage, setIdErrorMessage] = useState("아이디를 입력해 주세요.");
+  const [idValidMessage, setIdValidMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("비밀번호를 입력해주세요.");
+  const [passwordValidMessage, setPasswordValidMessage] = useState("");
+  const [password2ErrorMessage, setPassword2ErrorMessage] = useState("비밀번호 확인을 입력해주세요.");
+  const [password2ValidMessage, setPassword2ValidMessage] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
 
+  const idValid = (id) => {
+    return id.length >= 6 && id.length <= 16 && !/[\u3131-\uD79D]/.test(id) && id !== "";
+  };
+
+  const passwordValid = (password) => {
+    return password.length >= 8 && password.length <= 16 && password !== "";
+  };
+
+  const password2Valid = (password2) => {
+    return password === password2 && password2 !== "";
+  };
+  
   const handleIdInputChange = (event) => {
     // 정규 표현식을 사용하여 한글 입력을 필터링하고 필요에 따라 수정
     const value = event.target.value;
@@ -14,29 +33,69 @@ function Signin(props) {
       alert('아이디는 한글을 입력 하실 수 없습니다');
       return;
     }
-    setId(value);
-  }
+    setId(value); // 아이디 상태 업데이트
+    
+    
+    // 값이 변경되었을 때만 에러 메시지 업데이트
+    if (value === "") {
+      setIdErrorMessage("아이디를 입력해 주세요.");
+    } else if (idValid(value)) {
+      setIdErrorMessage("");
+      setIdValidMessage("아이디가 올바릅니다.");
+    } else {
+      setIdErrorMessage("아이디 형식이 올바르지 않습니다.");
+      setIdValidMessage("");
+    }
+  };
 
   const handlePasswordInputChange = (event) => {
-    const inputValue = event.target.value;
+    const value = event.target.value;
+    setPassword(value); // 비밀번호 상태 업데이트
     // 유효성 검사 룰을 정의합니다.
-    if (inputValue.length >= 8 && inputValue.length <= 16) {
-      setPasswordErrorMessage("");
+    if (value === "") {
+      setPasswordErrorMessage("비밀번호를 입력해주세요.");
+    } else if (passwordValid(value)) {
+      setPasswordValidMessage("안전한 비밀번호입니다.");
+      setPasswordErrorMessage("")
     } else {
       setPasswordErrorMessage('비밀번호는 8글자 이상 16글자 이하로 입력해주세요.');
+      setPasswordValidMessage("");
     }
-    setPassword(inputValue); // 비밀번호 상태 업데이트
+
+    if(value === password2){
+      setPassword2ValidMessage("비밀번호가 일치합니다.");
+      setPassword2ErrorMessage("")
+    }else if(password2 !== ""){
+      setPassword2ErrorMessage('비밀번호가 일치하지 않습니다.');
+      setPassword2ValidMessage("");
+    }
+  }
+
+  const handlePassword2InputChange = (event) => {
+    const value = event.target.value;
+    setPassword2(value); // 비밀번호 상태 업데이트
+    // 유효성 검사 룰을 정의합니다.
+    if (value === "") {
+      setPassword2ErrorMessage("비밀번호 확인을 입력해주세요.");
+    } else if (password2Valid(value)) {
+      setPassword2ValidMessage("비밀번호가 일치합니다.");
+      setPassword2ErrorMessage("")
+    } else {
+      setPassword2ErrorMessage('비밀번호가 일치하지 않습니다.');
+      setPassword2ValidMessage("");
+    }
   }
 
   const handleEmailInputChange = (event) => {
     const value = event.target.value;
     // 유효성 검사 룰을 정의합니다.
     if (/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(value)) {
-      setEmailErrorMessage("");
+      setEmailErrorMessage("올바른 이메일 입니다.");
     } else {
       setEmailErrorMessage('이메일 형식으로 작성해 주세요');
     }
     setEmail(value); // 이메일 상태 업데이트
+    setEmailErrorMessage(email.length === 0 ? "이메일을 입력해주세요." : "");
   }
 
   // const handleSubmit = async () => {
@@ -108,27 +167,43 @@ function Signin(props) {
             <form className="wrapper-box" role="form" id='signin-form'>
               <div className="input-box">
                 <input type="text"
-                  className="form-control form-control-email"
+                  className="form-control form-control-id"
                   placeholder="아이디를 입력해주세요"
                   value={id}
                   onChange={handleIdInputChange}
                 />
+                <div>
+                  {idErrorMessage && (
+                    <p style={{ color: 'red', fontSize: '11px' }}>{idErrorMessage}</p>
+                  )}
+                  {idValidMessage && (
+                    <p style={{ color: 'green', fontSize: '11px' }}>{idValidMessage}</p>
+                  )}
+                </div>
                 <input type="password"
                   className="form-control form-control-password"
                   placeholder="비밀번호를 입력해주세요"
                   value={password}
                   onChange={handlePasswordInputChange}
                 />
-                {passwordErrorMessage && (
-                  <p style={{ color: 'red', 
-                              fontSize: '13px'}}>{passwordErrorMessage}</p>
-                )}
+                  {passwordErrorMessage && (
+                    <p style={{ color: 'red', fontSize: '11px' }}>{passwordErrorMessage}</p>
+                  )}
+                  {passwordValidMessage && (
+                    <p style={{ color: 'green', fontSize: '11px' }}>{passwordValidMessage}</p>
+                  )}
                 <input type="password"
                   className="form-control form-control-password"
                   placeholder="비밀번호를 다시 입력해주세요"
-                  onChange={event => {
-                    setPassword2(event.target.value);
-                  }} />
+                  value={password2}
+                  onChange={handlePassword2InputChange} 
+                />
+                  {password2ErrorMessage && (
+                    <p style={{ color: 'red', fontSize: '11px' }}>{password2ErrorMessage}</p>
+                  )}
+                  {password2ValidMessage && (
+                    <p style={{ color: 'green', fontSize: '11px' }}>{password2ValidMessage}</p>
+                  )}
                 <div>
                   <input type="email"
                     className="form-control form-control-email"
@@ -136,13 +211,14 @@ function Signin(props) {
                     required
                     value={email}
                     onChange={handleEmailInputChange}
-                  />{emailErrorMessage && (
-                    <p style={{ color: 'red', 
-                                fontSize: '13px'}}>{emailErrorMessage}</p>
-                  )}
+                  />
                   <button type='button'>
                     인증번호 발송
                   </button>
+                  {emailErrorMessage && (
+                    <p style={{ color: 'red', 
+                                fontSize: '13px'}}>{emailErrorMessage}</p>
+                  )}
                 </div>
               </div>
 
