608706b01ecbafdb7de3b6cc996467e97de23c60
junggwanho <rhksgh1222@naver.com>
2023-11-18 18:52:40 +0900

    아 개 귀찮게 하네

--- src/document/Main.js
+++ src/document/Main.js
@@ -13,26 +13,38 @@ export default function Main() {
             {[false].map((expand) => (
             <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" bg="dark" data-bs-theme="dark" sticky="top">
                 <Container fluid>
-                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                <Navbar.Brand href="#">내용 입력</Navbar.Brand>
-                <Navbar.Offcanvas
-                    id={`offcanvasNavbar-expand-${expand}`}
-                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                    >
-                    <Offcanvas.Header closeButton>
-                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                    Offcanvas
-                    </Offcanvas.Title>
-                </Offcanvas.Header>
-                        {/* <Offcanvas.Body> */}
-                        {/* <div className="background">
-                            mode 상태에 따라 Login, Signin, 또는 FindId 컴포넌트를 렌더링합니다.
-                            {mode === "MAIN" ? <Main setMode={setMode} /> : mode === "EMAIL" ? <Email setMode={setMode} /> : mode === "SIGNIN" ? <Signin setMode={setMode} /> : <FindPw setMode={setMode} />}
-                        </div> */}
-                            <SideOpenbar/>
-                        {/* </Offcanvas.Body> */}
-                <Nav className="justify-content-end flex-grow-1 pe-3"></Nav>
-                </Navbar.Offcanvas>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-${expand}`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                        >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                            Offcanvas
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                                {/* <Offcanvas.Body> */}
+                                {/* <div className="background">
+                                    mode 상태에 따라 Login, Signin, 또는 FindId 컴포넌트를 렌더링합니다.
+                                    {mode === "MAIN" ? <Main setMode={setMode} /> : mode === "EMAIL" ? <Email setMode={setMode} /> : mode === "SIGNIN" ? <Signin setMode={setMode} /> : <FindPw setMode={setMode} />}
+                                </div> */}
+                                    <SideOpenbar/>
+                                {/* </Offcanvas.Body> */}
+                        <Nav className="justify-content-end flex-grow-1 pe-3"></Nav>
+                    </Navbar.Offcanvas>
                 </Container>
             </Navbar>))}
                     <Routes>
